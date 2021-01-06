7a46fa51e273efcc3da93057ede7885f82367a3a
kyler-swanson <kylerswanson@gmail.com>
2021-01-06 17:05:52 -0500

    Update searchMovies action to include more error checking (api doesn't return error codes!)

--- src/actions/movieActions.js
+++ src/actions/movieActions.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import getApiKey from '../util/getApiKey';
 import {
   SEARCH_MOVIES,
+  CLEAR_MOVIES,
   ADD_NOMINATION,
   DELETE_NOMINATION,
   MOVIES_ERROR,
@@ -11,36 +12,49 @@ import {
 export const searchMovies = (text) => async (dispatch) => {
   try {
     setLoading();
-    const res = await axios.get(`/?apikey=${getApiKey()}&s=${text}`);
+    const res = await axios.get(
+      `https://www.omdbapi.com/?apikey=${getApiKey()}&s=${text}`
+    );
 
-    dispatch({
-      type: SEARCH_MOVIES,
-      payload: res.data.Search
-    });
+    if (text.length === 0) {
+      dispatch({
+        type: CLEAR_MOVIES
+      });
+    } else if ('Error' in res.data) {
+      dispatch({
+        type: MOVIES_ERROR,
+        payload: res.data.Error
+      });
+    } else {
+      dispatch({
+        type: SEARCH_MOVIES,
+        payload: res.data.Search
+      });
+    }
   } catch (err) {
     dispatch({
       type: MOVIES_ERROR,
-      payload: err.response
+      payload: err
     });
   }
 };
 
-export const addNomination = (movie) => {
-  return {
+export const addNomination = (movie) => (dispatch) => {
+  dispatch({
     type: ADD_NOMINATION,
     payload: movie
-  };
+  });
 };
 
-export const deleteNomination = (movie) => {
-  return {
+export const deleteNomination = (movie) => (dispatch) => {
+  dispatch({
     type: DELETE_NOMINATION,
     payload: movie
-  };
+  });
 };
 
-export const setLoading = () => {
-  return {
+export const setLoading = () => (dispatch) => {
+  dispatch({
     type: SET_LOADING
-  };
+  });
 };
