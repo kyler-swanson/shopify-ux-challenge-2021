d0d4c910b7d1094614d96de6daca0e44f6e7b69d
kyler-swanson <kylerswanson@gmail.com>
2021-01-06 17:21:21 -0500

    Add Nomination notification, and update nominate button accordingly

--- src/components/movies/MovieItem.js
+++ src/components/movies/MovieItem.js
@@ -1,25 +1,44 @@
 import React, { Fragment } from 'react';
+import { connect } from 'react-redux';
+import { addNomination } from '../../actions/movieActions';
 import PropTypes from 'prop-types';
 
-import { Typography, Avatar, Space, Button, Tooltip } from 'antd';
-import { LikeOutlined } from '@ant-design/icons';
+import { Typography, Avatar, Space, Button, Tooltip, notification } from 'antd';
+import { LikeOutlined, SmileOutlined } from '@ant-design/icons';
 const { Text } = Typography;
 
-const MovieItem = ({ movie }) => {
+const MovieItem = ({ movie, nominations, addNomination }) => {
+  const onNomination = () => {
+    addNomination(movie);
+
+    notification['success']({
+      message: 'Movie Nominated',
+      description: (
+        <span>
+          You've nominated <strong>{movie.Title}</strong> for the Shoppies!
+        </span>
+      )
+    });
+  };
+
   return (
     <Fragment>
       <Space align='center'>
         <Avatar src={movie.Poster} />
-        <Text ellipsis type='secondary'>
+        <Text type='secondary'>
           <Text>{movie.Title}</Text> {movie.Year}
         </Text>
       </Space>
-      <Tooltip title='Nominate'>
+      <Tooltip title={nominations.includes(movie) ? 'Nominated!' : 'Nominate'}>
         <Button
           type='primary'
           style={{ float: 'right' }}
           shape='circle'
-          icon={<LikeOutlined />}
+          onClick={onNomination}
+          disabled={nominations.includes(movie)}
+          icon={
+            nominations.includes(movie) ? <SmileOutlined /> : <LikeOutlined />
+          }
         />
       </Tooltip>
     </Fragment>
@@ -27,7 +46,13 @@ const MovieItem = ({ movie }) => {
 };
 
 MovieItem.propTypes = {
-  movie: PropTypes.object.isRequired
+  movie: PropTypes.object.isRequired,
+  nominations: PropTypes.array,
+  addNomination: PropTypes.func.isRequired
 };
 
-export default MovieItem;
+const mapStateToProps = (state) => ({
+  nominations: state.movie.nominations
+});
+
+export default connect(mapStateToProps, { addNomination })(MovieItem);
