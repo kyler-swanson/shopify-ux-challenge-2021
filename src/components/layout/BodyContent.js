32b040f2b7ae539c3424e6567dca67c14758a620
kyler-swanson <kylerswanson@gmail.com>
2021-01-06 14:47:13 -0500

    Add Search component and basic layout to BodyContent

--- src/components/layout/BodyContent.js
+++ src/components/layout/BodyContent.js
@@ -1,15 +1,23 @@
 import React, { Fragment } from 'react';
 
 import Container from './Container';
-import { Layout } from 'antd';
+import { Layout, Divider, Row, Col } from 'antd';
+import Search from '../Search';
 
 const { Content } = Layout;
 
 const BodyContent = () => {
   return (
     <Fragment>
-      <Content>
-        <Container>Hello</Container>
+      <Content style={{ backgroundColor: 'white' }}>
+        <Container>
+          <Divider plain />
+          <Search />
+          <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+            <Col span={12}>Hello</Col>
+            <Col span={12}>Hello</Col>
+          </Row>
+        </Container>
       </Content>
     </Fragment>
   );
