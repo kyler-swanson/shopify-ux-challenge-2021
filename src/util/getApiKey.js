ab8f6100f05da92e5a752b87d8cc64dc80fd8053
kyler-swanson <kylerswanson@gmail.com>
2021-01-06 18:44:16 -0500

    Change api env var to default to REACT_APP_OMDB_API_KEY

--- src/util/getApiKey.js
+++ src/util/getApiKey.js
@@ -1,8 +1,4 @@
 // eslint-disable-next-line
 export default () => {
-  if (process.env.NODE_ENV !== 'production') {
-    return process.env.REACT_APP_OMDB_API_KEY;
-  } else {
-    return process.env.OMDB_API_KEY;
-  }
+  return process.env.REACT_APP_OMDB_API_KEY;
 };
