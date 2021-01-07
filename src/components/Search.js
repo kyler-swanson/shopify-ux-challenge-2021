e8cec1068a90d32e38afa5a6146019676a93ef1d
kyler-swanson <kylerswanson@gmail.com>
2021-01-06 20:48:33 -0500

    Add blur() call on search to hide keyboard on mobile

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { useRef } from 'react';
 import { connect } from 'react-redux';
 import { searchMovies } from '../actions/movieActions';
 import PropTypes from 'prop-types';
@@ -8,21 +8,23 @@ import { Input } from 'antd';
 const { Search: AntSearch } = Input;
 
 const Search = ({ searchMovies }) => {
+  const text = useRef('');
+
   const onSearch = (value) => {
+    text.current.blur();
     searchMovies(value);
   };
 
   return (
-    <Fragment>
-      <AntSearch
-        placeholder='Search movies...'
-        allowClear
-        enterButton
-        onSearch={onSearch}
-        size='large'
-        style={{ marginTop: '20px' }}
-      />
-    </Fragment>
+    <AntSearch
+      placeholder='Search movies...'
+      allowClear
+      enterButton
+      onSearch={onSearch}
+      size='large'
+      ref={text}
+      style={{ marginTop: '20px' }}
+    />
   );
 };
 
